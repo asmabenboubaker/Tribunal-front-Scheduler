184cc574d5de0c501c3a47f055ac2e14bada07f9
Asma Ben Boubaker <36500896+asmabenboubaker@users.noreply.github.com>
2023-08-11 16:26:32 +0200

    Localization

--- src/app/pages/scheduler/scheduler.component.ts
+++ src/app/pages/scheduler/scheduler.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit,ViewChild, enableProdMode  } from '@angular/core';
 import { Audience } from './Audience';
-// import serciescheduler from './service/servicescheduler.service';
 import { Resource, ServiceschedulerService } from '../../service/servicescheduler.service';
 import { DxButtonModule, DxSchedulerModule, DxSchedulerComponent } from "devextreme-angular";
 import CustomStore from 'devextreme/data/custom_store';
@@ -12,14 +11,24 @@ import DataSource from 'devextreme/data/data_source';
 import { LoadOptions } from 'devextreme/data';
 import ODataStore from 'devextreme/data/odata/store';
 import { ChangeDetectorRef } from '@angular/core';
+import { locale, loadMessages } from 'devextreme/localization';
+import arMessages from 'devextreme/localization/messages/ar.json';
+
+ 
+
+
 @Component({
   selector: 'app-scheduler',
   templateUrl: './scheduler.component.html',
   styleUrls: ['./scheduler.component.scss']
 })
 
-export class SchedulerComponent    {
-   
+export class SchedulerComponent   implements OnInit {
+  //arabic scheduler
+  ngOnInit() {
+    loadMessages(arMessages); 
+    locale('ar'); 
+  }
   resourcesData: Resource[];
 // add fields to the form
 onAppointmentFormCreated (e:any) {
@@ -69,103 +78,11 @@ onAppointmentFormCreated (e:any) {
   //customDataSource: CustomStore;
   customDataSource: CustomStore;
   productsStore: ODataStore;
-
-  // dataSource: Audience[] = [
-  //   {
-  //     title: 'Meeting 1',
-  //     startDate: new Date(2023, 7, 7, 10, 0), // Year, Month (0-indexed), Day, Hour, Minute
-  //     endDate: new Date(2023, 7, 7, 11, 30),
-  //     dayLong: false,
-  //   },
-  //   {
-  //     title: 'All-Day Event',
-  //     startDate: new Date(2023, 7, 8),
-  //     endDate: new Date(2023, 7, 9),
-  //     dayLong: true,
-  //   },
-  //   {
-  //     title: 'Recurring Event',
-  //     startDate: new Date(2023, 7, 9, 15, 0),
-  //     endDate: new Date(2023, 7, 9, 16, 30),
-  //     dayLong: false,
-  //     recurrence: 'FREQ=WEEKLY;BYDAY=TU;COUNT=5', // Recurs every Tuesday for 5 occurrences
-  //   },
-  // ];
  
   currentDate: Date = new Date(2021, 3, 27);
   appointmentsData: any;
-//   constructor(private dataService:ServiceschedulerService,private http: HttpClient) {
-     
-//       const url = 'http://localhost:8081/picosoft/api/schedule';
-   
-//       this.appointmentsData = AspNetData.createStore({
-//         key: 'AppointmentId',
-//         loadUrl: `${url}/loadData`,
-//         insertUrl: `${url}/addAudience`,
-//         updateUrl: `${url}/Put`,
-//         deleteUrl: `${url}/Delete`,
-        
-//         onBeforeSend(method, ajaxOptions) {
-          
-//           ajaxOptions.xhrFields = { withCredentials: true };
-      
-//         // i get bad request 
-//         //   ajaxOptions.data = {
-//         //     AppointmentId: 1,
-//         //     Title: "test",
-//         //     StartDate: new Date(2021, 3, 27),
-//         //     EndDate: new Date(2021, 3, 27),
-//         //     DayLong: false,
-//         //     Recurrence: "test",
-//         //     RecurrenceException: "test",
-//         //     RecurrenceID: 1,
-//         //     RecurrenceRule: "test",
-//         //     Description: "test",
-//         //     Location: "test",
-//         //     OwnerId: 1,
-//         //     Priority: 1,
-//         //     ReminderInfo: "test",
-//         //     ReminderTime: "test",
-//         //     AllDay: false,
-//         //     Type: "test",
-//         //     Status: "test",
-//         //     Label: "test",
-//         //     ResourceId: 1,
-//         //     ResourceIds: "test",
-//         //     Attendees: "test",
-//         //     AttendeesId: 1,
-          
-//         // };
-// // unsupportable media type
-        
-//         },
 
-//       });
-    
-// }
-    
-// store: CustomStore;
-// constructor() {
-//     let serviceUrl = "http://localhost:8081/picosoft/api/schedule";
-//     this.store = createStore({
-//         key: "AppointmentId",
-//         loadUrl: serviceUrl + "/loadData",
-     
 
-//         insertUrl: serviceUrl + "/addAudience",
-//         updateUrl: serviceUrl + "/UpdateAction",
-//         deleteUrl: serviceUrl + "/DeleteAction",
-//         onBeforeSend(method, ajaxOptions) {
-//           method = "POST";
-//           ajaxOptions.xhrFields = { withCredentials: true };
-//           ajaxOptions.dataType = "json";
-//           ajaxOptions.contentType = "application/json";
-      
-//         },
-        
-//     })
-    
-// }
 
  
 store: CustomStore;
